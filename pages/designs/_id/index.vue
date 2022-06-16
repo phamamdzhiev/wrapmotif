fb8df544547ae70c87474c6637f2b01f1b22a13b
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-06-16 15:02:27 +0300

    Revert some changes on item image slider

--- pages/designs/_id/index.vue
+++ pages/designs/_id/index.vue
@@ -39,7 +39,7 @@
       <div class="contain-gallery container-fluid mb-4 w-100">
         <div class="row">
           <!-- thumbnail -->
-          <div class="col-md-8 col-xl-9 w-100 p-0">
+          <div class="col-md-8 col-xl-9 w-100 p-0" style="background-color: #f1f1f1">
             <div class="w-100">
               <div>
                 <client-only>
