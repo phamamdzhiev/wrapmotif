bdb6f3a58628b16db8a10d4faa816f20c6a82bea
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-06-16 10:50:10 +0300

    Fix issue with cart and deleting the wrong item from it

--- store/cart.js
+++ store/cart.js
@@ -1,73 +1,76 @@
 export const state = () => ({
-    cart: []
+  cart: []
 });
 
 export const getters = {
-    getTotalPrice: state => {
-        let total = 0;
-        state.cart.filter(item => {
-            total += item.price;
-        });
-        return total;
-    },
+  getTotalPrice: state => {
+    let total = 0;
+    state.cart.filter(item => {
+      total += item.price;
+    });
+    return total;
+  },
 
-    getTotalItem: state => {
-        if (state.cart.length > 9) {
-            return "9+";
-        }
-        return state.cart.length;
-    },
+  getTotalItem: state => {
+    if (state.cart.length > 9) {
+      return "9+";
+    }
+    return state.cart.length;
+  },
 
-    getCartItems: state => state.cart
+  getCartItems: state => state.cart
 };
 
 export const mutations = {
-    ADD_TO_CART(state, item) {
-        let found = state.cart.find(product => product.id == item.id);
-        if (found) {
-            // Show toast
-        } else {
-            state.cart.push(item);
-        }
-    },
-
-    SAVE_DATA(state) {
-        localStorage.setItem("cart", JSON.stringify(state.cart));
-    },
-
-    REMOVE_FROM_CART(state, {cardID, url}) {
-        let index = state.cart.findIndex(e => e == cardID);
-        state.cart.splice(index, 1);
-    },
-
-    SET_CART(state) {
-        state.cart = localStorage.getItem("cart") ?
-            JSON.parse(localStorage.getItem("cart")) : [];
-    },
-
-    RESET_CART(state) {
-        localStorage.setItem("cart", []);
-        state.cart = [];
+  ADD_TO_CART(state, item) {
+    let found = state.cart.find(product => product.id == item.id);
+    if (found) {
+      // Show toast
+    } else {
+      state.cart.push(item);
     }
+  },
+
+  SAVE_DATA(state) {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  },
+
+  REMOVE_FROM_CART(state, {cardID, url}) {
+    let index = state.cart.findIndex(e => e == cardID);
+    let obj = state.cart.find(e => e == cardID);
+    console.log('Index', index);
+    console.log('Obejct cart', obj);
+    state.cart.splice(index, 1);
+  },
+
+  SET_CART(state) {
+    state.cart = localStorage.getItem("cart") ?
+      JSON.parse(localStorage.getItem("cart")) : [];
+  },
+
+  RESET_CART(state) {
+    localStorage.setItem("cart", []);
+    state.cart = [];
+  }
 };
 
 // Actions
 export const actions = {
-    addToCart({ commit }, payload) {
-        commit("ADD_TO_CART", payload);
-        commit("SAVE_DATA", payload);
-    },
+  addToCart({commit}, payload) {
+    commit("ADD_TO_CART", payload);
+    commit("SAVE_DATA", payload);
+  },
 
-    removeFromCart({ commit }, payload) {
-        commit("REMOVE_FROM_CART", payload);
-        commit("SAVE_DATA", payload);
-    },
+  removeFromCart({commit}, payload) {
+    commit("REMOVE_FROM_CART", payload);
+    commit("SAVE_DATA", payload);
+  },
 
-    setCart({ commit }) {
-        commit("SET_CART");
-    },
+  setCart({commit}) {
+    commit("SET_CART");
+  },
 
-    resetCart({ commit }) {
-        commit("RESET_CART");
-    }
+  resetCart({commit}) {
+    commit("RESET_CART");
+  }
 };
