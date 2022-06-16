eecbb1aca4065e475665dc2edb6df7ee4169cbfd
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-06-16 23:21:33 +0300

    Render about us page content

--- pages/profile/index/index.vue
+++ pages/profile/index/index.vue
@@ -1,38 +1,40 @@
 <template>
-	<div>
-		<div class="myProfile mb-4">
-			<ul class="nav nav-tabs px-2">
-				<li class="nav-item">
-					<nuxt-link to="/profile" class="nav-link text-uppercase">Profile Settings</nuxt-link>
-				</li>
-				<li class="nav-item">
-					<nuxt-link to="/profile/orders" class="nav-link text-uppercase">Purchase orders</nuxt-link>
-				</li>
-				<li class="nav-item">
-					<nuxt-link to="/profile/designs" class="nav-link text-uppercase
-          ">My designs</nuxt-link>
-				</li>
-				<li class="nav-item">
-					<nuxt-link to="/profile/color-changes" class="
+  <div>
+    <div class="myProfile mb-4">
+      <ul class="nav nav-tabs px-2">
+        <li class="nav-item">
+          <nuxt-link to="/profile" class="nav-link text-uppercase">Profile Settings</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/profile/orders" class="nav-link text-uppercase">Purchase orders</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/profile/designs" class="nav-link text-uppercase
+          ">My designs
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/profile/color-changes" class="
             nav-link text-uppercase
-          ">Color Changes</nuxt-link>
-				</li>
-				<li class="nav-item">
-					<nuxt-link to="/profile/preview-designs" class="nav-link text-uppercase">Preview Designs</nuxt-link>
-				</li>
-				<li class="nav-item">
-					<nuxt-link to="/profile/custom-orders" class="nav-link text-uppercase">Custom Orders</nuxt-link>
-				</li>
-				<li class="nav-item">
-					<nuxt-link to="/profile/favourite" class="nav-link text-uppercase">My favorite</nuxt-link>
-				</li>
-			</ul>
-		</div>
+          ">Color Changes
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/profile/preview-designs" class="nav-link text-uppercase">Preview Designs</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/profile/custom-orders" class="nav-link text-uppercase">Custom Orders</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to="/profile/favourite" class="nav-link text-uppercase">My favorite</nuxt-link>
+        </li>
+      </ul>
+    </div>
 
-		<div class="my-3">
-			<nuxt-child></nuxt-child>
-		</div>
-	</div>
+    <div class="my-3">
+      <nuxt-child></nuxt-child>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -41,28 +43,30 @@ export default {};
 
 <style lang="scss" scoped>
 .nuxt-link-active {
-	color: black !important;
+  color: black !important;
 }
+
 .nuxt-link-exact-active {
-  background-image: linear-gradient(#6F3A1F 0%, #DC7B54 85%)!important;
-  -webkit-background-clip: text!important;
-  -webkit-text-fill-color: transparent!important;
+  border-bottom: 3px solid var(--primary) !important;
+  background-image: linear-gradient(#6F3A1F 0%, #DC7B54 85%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 
 .nav-tabs {
-	border-bottom: 1px solid #dee2e6;
+  border-bottom: 1px solid #dee2e6;
 }
 
 .nav-tabs .nav-item {
-	margin-bottom: -1px;
+  margin-bottom: -1px;
 }
 
 .nav-link {
-	border: none;
-	padding: 16px;
-	padding-bottom: 12px;
-	font-size: 1rem;
-	text-transform: uppercase;
-	font-weight: 600;
+  border: none;
+  padding: 16px;
+  padding-bottom: 12px;
+  font-size: 1rem;
+  text-transform: uppercase;
+  font-weight: 600;
 }
 </style>
