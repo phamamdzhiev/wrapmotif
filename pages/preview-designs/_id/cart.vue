9ba8fa0ce280bf963dd676e57704f7abfab402ac
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-06-26 12:51:53 +0300

    das

--- pages/preview-designs/_id/cart.vue
+++ pages/preview-designs/_id/cart.vue
@@ -362,7 +362,7 @@ export default {
           });
         console.log('----- GET PREVIEW SUCCEESS RESPONSE ----', res.data)
         this.loading = false;
-        this.sessionId = res.data.id;
+        this.sessionId = 1;
       } catch (e) {
         this.loading = false;
         console.log('----- GET PREVIEW ERROR RESPONSE ----', e.response);
