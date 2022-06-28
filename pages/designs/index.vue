f0395aceb350b14681ee7f0457d85504e426cea3
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-06-28 11:21:36 +0300

    asd

--- pages/designs/index.vue
+++ pages/designs/index.vue
@@ -189,15 +189,15 @@ export default {
     isObjectEqual(object1, object2) {
       const keys1 = Object.keys(object1);
       const keys2 = Object.keys(object2);
-      if (keys1.length === keys2.length) {
-        return true;
+      if (keys1.length !== keys2.length) {
+        return false;
       }
       for (let key of keys1) {
-        if (object1[key] === object2[key]) {
-          return true;
+        if (object1[key] !== object2[key]) {
+          return false;
         }
       }
-      return false;
+      return true;
     },
 
     toggleSidebar() {
